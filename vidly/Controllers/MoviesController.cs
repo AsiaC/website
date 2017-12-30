d63098346043fa187be2d66825e7cd84441f2a54
AsiaC <czaplicka.asia@gmail.com>
2017-12-30 21:48:33 +0100

    add first files

---
+++ vidly/Controllers/MoviesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using vidly.Models;
+
+namespace vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        /*public ActionResult Index()
+        {
+            return View();
+        }*/
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        //public ViewResult Random()
+        {
+            //instancja MovieModel
+            var movie = new Movie() { Name = "Shrek!" };
+            return View(movie);
+            // return new ViewResult(); - inny sposób
+            //return Content("Hello world!!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new { page=1,sortBy="name"}); //http://localhost:52701/?page=1&sortBy=name
+        }
+    }
+}
